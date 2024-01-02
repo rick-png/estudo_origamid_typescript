772f5feff5e14b5cc28047e3b7298a07b9a9addc
Rick-png <enricksantos03@gmail.com>
2024-01-01 22:25:51 -0300

    0102 Types

---
+++ 01 TypeScript para Iniciantes/0102 Types/script.js
@@ -0,0 +1,11 @@
+//@ts-check
+const frase = 'Front End';
+const total = 100.26;
+
+frase.toLowerCase();
+// total.toLowerCase(); erro
+
+const t = total.toFixed();
+
+console.log(t + 10);
+// t virou string
