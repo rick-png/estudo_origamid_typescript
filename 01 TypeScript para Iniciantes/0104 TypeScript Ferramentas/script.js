3dade3960980dbf17a9a058005c859705a8dc84c
Rick-png <enricksantos03@gmail.com>
2024-01-01 22:53:23 -0300

    0104 TypeScript Ferramentas

--- 01 TypeScript para Iniciantes/0104 TypeScript Ferramentas/script.js
+++ 01 TypeScript para Iniciantes/0104 TypeScript Ferramentas/script.js
@@ -1,19 +1,5 @@
-const produtos = [{
-    nome: 'O Senhor dos Anéis',
-    tipo: 'livro',
-  },
-  {
-    nome: 'A Guerra dos Tronos',
-    tipo: 'livro',
-  },
-  {
-    nome: 'Dark Souls',
-    tipo: 'jogo',
-  },
-];
-
-function filtrarLivros(dados) {
-  return dados.filter(item => item.tipo === 'livro');
+"use strict";
+function somar(a, b) {
+    return a + b;
 }
-
-console.log(filtrarLivros(produtos))
+console.log(somar(5, 20));
